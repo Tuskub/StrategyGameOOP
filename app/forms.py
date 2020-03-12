dff74b3598c89398c0c2147416c9cd878f26b9fa
Tuskub <56831532+Tuskub@users.noreply.github.com>
2020-03-12 20:03:29 +0500

    Реализация формы и валидация данных

---
+++ app/forms.py
@@ -0,0 +1,32 @@
+import re
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, ValidationError
+
+
+class ActionForm(FlaskForm):
+    unit = StringField('Выбор юнита', validators=[DataRequired()])
+    action = SelectField('Действие', choices=[('move', 'Перейти'), ('attack', 'Атаковать')]) # noqa
+    target = StringField('Выбор цели', validators=[DataRequired()])
+    submit = SubmitField('Сходить')
+
+    def validate_unit(self, unit):
+        nums = re.findall(r'\d{1,2}', unit.data)
+        out_of_map = False
+        for num in nums:
+            if int(num) < 1 or int(num) >= 19:
+                out_of_map = True
+        print(nums)
+        if not re.match(r'^[[(]?\d{1,2}[,\s][\s]?\d{1,2}[])]?$', unit.data) or out_of_map:
+            raise ValidationError('Введены неверные координаты.')
+
+    def validate_target(self, target):
+        nums = re.findall(r'\d{1,2}', target.data)
+        out_of_map = False
+        for num in nums:
+            print(int(num))
+            if int(num) < 1 or int(num) >= 19:
+                out_of_map = True
+        print(nums)
+        if not re.match(r'^[[(]?\d{1,2}[,\s][\s]?\d{1,2}[])]?$', target.data) or out_of_map:
+            raise ValidationError('Введены неверные координаты.')
